94d43390f627af33ac1bedb4f5e7fc6868436edc
gnstncbc <gnstncbc@gmail.com>
2025-05-11 21:32:05 +0300

    api change

--- src/components/Carpet/api.js
+++ src/components/Carpet/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // --- API Base URL ---
-// const API_BASE_URL = 'https://gnstncbc.com/api'; // Production
-export const API_BASE_URL = 'http://localhost:8080/api'; // Local development
+const API_BASE_URL = 'https://gnstncbc.com/api'; // Production
+// export const API_BASE_URL = 'http://localhost:8080/api'; // Local development
 
 // --- API Client (Axios Instance) ---
 export const apiClient = axios.create({
