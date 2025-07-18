17d734dffcd7f4ebf5959096987c7b6ba7aa503d
Güneş Tan Cebeci <gnstncbc@gmail.com>
2025-07-19 01:10:14 +0300

    websocket v2

--- src/components/Carpet/api.js
+++ src/components/Carpet/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 // --- API Base URL ---
-// const API_BASE_URL = 'https://gnstncbc.com/api'; // Production
-export const API_BASE_URL = 'http://localhost:8080/api'; // Local development
+const API_BASE_URL = 'https://gnstncbc.com/api'; // Production
+// export const API_BASE_URL = 'http://localhost:8080/api'; // Local development
 
 // --- API Client (Axios Instance) ---
 export const apiClient = axios.create({
